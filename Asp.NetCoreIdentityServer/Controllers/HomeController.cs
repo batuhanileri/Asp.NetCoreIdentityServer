58ce97f6f5472c75c22669676be4353a9f0293ff
batu_ <batu_@DESKTOP-AOMM71G>
2022-02-23 15:10:10 +0300

    SignUp Added

--- Asp.NetCoreIdentityServer/Controllers/HomeController.cs
+++ Asp.NetCoreIdentityServer/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCoreIdentityServer.Models;
+using Asp.NetCoreIdentityServer.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,55 @@ namespace Asp.NetCoreIdentityServer.Controllers
 {
     public class HomeController : Controller
     {
+        private UserManager<AppUser> _userManager { get; }
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserViewModel userViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser();
+                user.UserName = userViewModel.UserName;
+                user.Email = userViewModel.Email;
+                user.PhoneNumber = userViewModel.PhoneNumber;
+
+                IdentityResult result = await _userManager.CreateAsync(user, userViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+
+                else
+                {
+                    foreach (IdentityError item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+
+
+            }
+
+            return View(userViewModel);
         }
     }
 }
