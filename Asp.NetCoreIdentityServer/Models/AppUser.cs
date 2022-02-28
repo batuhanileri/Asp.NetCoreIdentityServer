70f9ee1c03467f9eb480fadc814352c1b51aa05b
batu_ <batu_@DESKTOP-AOMM71G>
2022-02-28 10:46:42 +0300

    Add Additional Column To User Table

--- Asp.NetCoreIdentityServer/Models/AppUser.cs
+++ Asp.NetCoreIdentityServer/Models/AppUser.cs
@@ -8,6 +8,9 @@ namespace Asp.NetCoreIdentityServer.Models
 {
     public class AppUser : IdentityUser
     {
-       
+        public string City { get; set; }
+        public string Picture { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public int Gender { get; set; }
     }
 }
--- Asp.NetCoreIdentityServer/ViewModels/UserViewModel.cs
+++ Asp.NetCoreIdentityServer/ViewModels/UserViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Asp.NetCoreIdentityServer.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -24,6 +25,18 @@ namespace Asp.NetCoreIdentityServer.ViewModels
         [Display(Name = "Şifre: ")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+
+        [Display(Name = "Şehir: ")]
+        public string City { get; set; }
+        public string Picture { get; set; }
+
+        [Display(Name = "Doğum Tarihi: ")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDay { get; set; }
+
+        [Display(Name = "Cinsiyet: ")]
+        public Gender Gender { get; set; }
 
     }
 }
